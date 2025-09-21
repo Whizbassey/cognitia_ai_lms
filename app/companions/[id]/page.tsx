596d3910520ff45c70478155c0c923ebc9f3bee4
Wisdom Bassey <Lilwiziweb@gmail.com>
2025-09-21 17:42:22 +0100

    Implemented AI Companion Conversation

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CompanionComponent from "@/components/CompanionComponent";
 import { getCompanion } from "@/lib/actions/companions.actions";
 import { getSubjectColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
@@ -10,9 +11,12 @@ interface CompanionSessionPageProps {
 
 const CompanionSession = async ({params}: CompanionSessionPageProps) => {
     const { id } = await params;
-    const {name, subject, title, topic, duration} = await getCompanion(id);
+    const companion = await getCompanion(id);
     const user = await currentUser();
 
+    const {name, subject, title, topic, duration} = companion;
+    
+    
     if(!user) redirect('/sign-in');
     if(!name) redirect('/companions');
 
@@ -49,6 +53,13 @@ const CompanionSession = async ({params}: CompanionSessionPageProps) => {
               </div>
 
         </article>
+
+        <CompanionComponent
+            {...companion}
+            conpanionId={id}
+            userName={user.firstName!}
+            userImage={user.imageUrl!}
+        />
 
     </main>
     )
